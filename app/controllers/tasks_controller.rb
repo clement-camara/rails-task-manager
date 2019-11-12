3d3f66456454d54c1380aa3c815eb684b8f5f3b7
clement camara <clementcamara@gmail.com>
2019-11-12 22:13:38 +0100

    exo2

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,23 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params[:task])
+    @task.save
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # lire les tasks
+  get "tasks", to: "tasks#index"
+  # lire une task
+  get "tasks/new", to: "tasks#new"
+  get "tasks/:id", to: "tasks#show", as: :task
+  # add a task
+
+  post "tasks", to: "tasks#create"
+  # edith a task
+  get "tasks/:id/edith", to: "tasks#edit"
+  patch "tasks/id", to: "tasks#update"
+  # suprimer une task
+  delete "tasks/:id", to: "tasks#destroy"
 end
